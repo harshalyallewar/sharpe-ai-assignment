c7623aaee293f0bc662a8336f8f1e232946e3210
harshalyallewar <harshalyallewar2@gmail.com>
2023-12-02 23:00:23 +0530

    minor bug fixes

--- src/pages/Data/Data.jsx
+++ src/pages/Data/Data.jsx
@@ -1,6 +1,7 @@
 // Import necessary modules
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { ResponsiveContainer, PieChart, Pie, Cell, Legend } from "recharts";
 
 // Import component styling
 import "./data.css";
@@ -10,6 +11,17 @@ function Data() {
   // State to store fetched data and loading status
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const totalPosts = data.length;
+  const user1Posts = data.filter((d) => d.userId === 1).length;
+  const user1Percentage = (user1Posts / totalPosts) * 100;
+
+  const pieChartData = [
+    { name: "User ID 1", value: user1Percentage },
+    { name: "Other Users", value: 100 - user1Percentage },
+  ];
+
+  const COLORS = ["#0088FE", "#00C49F"];
 
   // useEffect to fetch data from the API on component mount
   useEffect(() => {
@@ -23,6 +35,30 @@ function Data() {
   // JSX rendering based on loading status
   return (
     <section className="posts">
+      <div className="pie-chart-container">
+        <h2>Pie Chart</h2>
+        <ResponsiveContainer width="100%" height={300}>
+          <PieChart>
+            <Pie
+              data={pieChartData}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              fill="#8884d8"
+              label
+            >
+              {pieChartData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Legend />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
       {loading ? (
         // Loading state display
         <div>Loading...</div>
